f7aaaf1d063144402f3f3a5382949f0ba8b43781
Daniel Kavanagh <dan.kava13@gmail.com>
2017-12-07 16:00:06 -0800

    add length validation to note schema

--- server/models/note.js
+++ server/models/note.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const NoteSchema = new Schema({
-    content: String
+    content: {type: String, required: true, minlength: 3}
 }, {timestamps: true});
 
 mongoose.model("Note", NoteSchema);
